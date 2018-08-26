2ba9e07c7720fc5c7fdd5c69593de75ee88276ae
AgeOfWar <palazzomichi@gmail.com>
2018-08-26 18:14:46 +0200

    Rename test

--- src/test/java/me/palazzomichi/telegram/telejam/objects/InlineKeyboardMarkupTest.java
+++ src/test/java/me/palazzomichi/telegram/telejam/objects/InlineKeyboardMarkupTest.java
@@ -7,10 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class InlineKeyboardMarkupTest {
   
   @Test
-  public void ofColumns() {
+  public void fromColumns() {
     InlineKeyboardButton button1 = new CallbackDataInlineKeyboardButton("hello", "sayhello");
     InlineKeyboardButton button2 = new SwitchInlineQueryCurrentChatInlineKeyboardButton("test", "");
-    InlineKeyboardMarkup keyboard1 = InlineKeyboardMarkup.ofColumns(
+    InlineKeyboardMarkup keyboard1 = InlineKeyboardMarkup.fromColumns(
         3, button1, button1, button2, button1, button2
     );
     InlineKeyboardMarkup keyboard2 = new InlineKeyboardMarkup(
