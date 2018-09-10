a573b470a94696fd368d6094fa92b63a72bffa97
AgeOfWar <palazzomichi@gmail.com>
2018-09-10 17:56:43 +0200

    Add logger to LongPollingBot

--- src/main/java/me/palazzomichi/telegram/telejam/LongPollingBot.java
+++ src/main/java/me/palazzomichi/telegram/telejam/LongPollingBot.java
@@ -1,9 +1,21 @@
 package me.palazzomichi.telegram.telejam;
 
+import me.palazzomichi.telegram.telejam.objects.Update;
 import me.palazzomichi.telegram.telejam.util.UpdateReader;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 import java.util.function.LongUnaryOperator;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import static me.palazzomichi.telegram.telejam.json.Json.toJson;
 
 /**
  * Runnable that receives and handles updates of a bot.
@@ -12,7 +24,57 @@ import java.util.function.LongUnaryOperator;
  */
 public abstract class LongPollingBot extends TelegramBot implements Runnable, AutoCloseable {
   
+  private final Logger logger;
   private final UpdateReader updateReader;
+  
+  private static Logger newLogger(Bot bot) {
+    Logger logger = Logger.getLogger(bot.getUsername());
+    logger.setUseParentHandlers(false);
+    ConsoleHandler handler = new ConsoleHandler();
+    handler.setFormatter(new Formatter() {
+      @Override
+      public String format(LogRecord record) {
+        String time = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS).toString();
+        String level = record.getLevel().getLocalizedName();
+        String text = formatMessage(record);
+        String message = "[" + time + "][" + level + "] " + text + System.lineSeparator();
+        Throwable t = record.getThrown();
+        if (t != null) {
+          StringWriter stringWriter = new StringWriter();
+          PrintWriter writer = new PrintWriter(stringWriter);
+          writer.println();
+          t.printStackTrace(writer);
+          message += stringWriter.toString();
+        }
+        return message;
+      }
+    });
+    logger.addHandler(handler);
+    return logger;
+  }
+  
+  /**
+   * Constructs a long polling bot.
+   *
+   * @param bot     the bot used by the reader
+   * @param backOff back off to be used when long polling fails
+   * @param logger  optional logger
+   */
+  public LongPollingBot(Bot bot, LongUnaryOperator backOff, Logger logger) {
+    super(bot);
+    this.logger = Objects.requireNonNull(logger);
+    updateReader = new UpdateReader(bot, backOff);
+  }
+  
+  /**
+   * Constructs a long polling bot.
+   *
+   * @param bot    the bot used by the reader
+   * @param logger optional logger
+   */
+  public LongPollingBot(Bot bot, Logger logger) {
+    this(bot, a -> 500, logger);
+  }
   
   /**
    * Constructs a long polling bot.
@@ -21,8 +83,7 @@ public abstract class LongPollingBot extends TelegramBot implements Runnable, Au
    * @param backOff back off to be used when long polling fails
    */
   public LongPollingBot(Bot bot, LongUnaryOperator backOff) {
-    super(bot);
-    updateReader = new UpdateReader(bot, backOff);
+    this(bot, backOff, newLogger(bot));
   }
   
   /**
@@ -31,26 +92,47 @@ public abstract class LongPollingBot extends TelegramBot implements Runnable, Au
    * @param bot the bot used by the reader
    */
   public LongPollingBot(Bot bot) {
-    this(bot, a -> 500L);
+    this(bot, a -> 500L, newLogger(bot));
   }
   
   @Override
   public void run() {
+    logger.info("Starting " + bot.getUsername() + "...");
     try {
+      logger.info("Discarding previous updates...");
       updateReader.discardAll();
     } catch (IOException e) {
+      logError(e);
       onError(e);
     }
-
+    logger.info("Waiting for updates...");
     while (!Thread.interrupted()) {
       try {
-        onUpdate(updateReader.read());
+        Update update = updateReader.read();
+        logger.finer("New update: " + toJson(update));
+        onUpdate(update);
       } catch (InterruptedException e) {
         break;
       } catch (Throwable e) {
+        logger.warning(e.getLocalizedMessage());
         onError(e);
       }
     }
+    logger.info("Stopping " + bot.getUsername() + "...");
+  }
+  
+  private void logError(Throwable t) {
+    logger.warning(t.getLocalizedMessage());
+    StackTraceElement[] stackTrace = t.getStackTrace();
+    if (stackTrace.length > 0) {
+      logger.throwing(stackTrace[0].getClassName(), stackTrace[0].getMethodName(), t);
+    } else {
+      logger.throwing("Unknown", "Unknown", t);
+    }
+  }
+  
+  @Override
+  public void onError(Throwable t) {
   }
   
   @Override
@@ -60,6 +142,15 @@ public abstract class LongPollingBot extends TelegramBot implements Runnable, Au
     } catch (IOException e) {
       throw new IOException("Unable to close LongPollingBot", e);
     }
+  }
+  
+  /**
+   * Returns the logger of the LongPollingBot.
+   *
+   * @return the logger this LongPollingBot
+   */
+  public Logger getLogger() {
+    return logger;
   }
   
 }
